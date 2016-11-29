6830ec3eff1aecd672a08bcacbda5a0aa0ebb3cb
ifotn <rich@infrontofthenet.com>
2016-11-29 08:24:40 -0500

    Add project files.

---
+++ Week12/Controllers/StoreController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Week12.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Store/Browse
+        public ActionResult Browse()
+        {
+            // create array of hard-coded genres
+            var genres = new string[] { "Rock", "Jazz", "Metal" };
+
+            ViewBag.genres = genres;
+            return View();
+        }
+
+        // GET: /Store/Details
+        public String Details(int id)
+        {
+            string message = "Store.Details, ID = " + id;
+            return message;
+        }
+    }
+}
